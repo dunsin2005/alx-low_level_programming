01d96a2460085680a63c360577d04e4a97e6e418
Awotinde Racheal <dunsinawotinde2005@gmail.com>
2023-06-22 16:49:49 -0700

    using the while loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - printing alphabets using putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+char x;
+
+x = 'a';
+while
+	(x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+putchar('\n');
+	return (0);
+}
