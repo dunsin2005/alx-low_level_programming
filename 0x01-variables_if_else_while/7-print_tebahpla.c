2aca034900053943908da2c0c63b573e5100be09
Awotinde Racheal <dunsinawotinde2005@gmail.com>
2023-06-22 19:22:32 -0700

    revere alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints alphabety in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char x;
+
+	x = 'z';
+	while
+		(x >= 'a')
+		{
+			putchar (x);
+			x--;
+		}
+	putchar ('\n');
+	return (0);
+}
