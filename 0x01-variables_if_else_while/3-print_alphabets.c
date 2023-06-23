24de2ea0f6638fa0d2d4b328adfa7f331cff1425
Awotinde Racheal <dunsinawotinde2005@gmail.com>
2023-06-22 18:18:30 -0700

    using while loop

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints ailphabet in lowercase and uppercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char x;
+
+	char y;
+
+	x = 'a';
+	y = 'A';
+	while
+		(x <= 'z')
+		{
+			putchar (x);
+			x++;
+		}
+	while
+		(y <= 'Z')
+		{
+			putchar (y);
+			y++;
+		}
+	putchar ('\n');
+	return (0);
+}
