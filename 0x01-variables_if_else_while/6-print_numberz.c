c806cbfb09b8d974f1790bad9ed3cd679635af79
Awotinde Racheal <dunsinawotinde2005@gmail.com>
2023-06-23 02:11:46 -0700

    while loop

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 0 to 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+		while
+			(i < 10)
+			{
+				putchar(i + '0');
+				i++;
+			}
+	putchar ('\n');
+	return (0);
+}
