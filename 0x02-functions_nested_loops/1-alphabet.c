1ea7a14591cd126d661342148b5ac4f0e3b25f0a
Awotinde Racheal <dunsinawotinde2005@gmail.com>
2023-06-24 16:16:05 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * print_alphabet - prints alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	x = 'a';
+	while
+		(x <= 'z')
+		{
+			putchar(x);
+			x++;
+		}
+	putchar('\n');
+}
