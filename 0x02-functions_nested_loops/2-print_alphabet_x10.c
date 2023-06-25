64e23367c90400b746433f6cd24f3226f7969ed7
Awotinde Racheal <dunsinawotinde2005@gmail.com>
2023-06-24 17:09:45 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets in lowercase 10 times
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char j;
+	int i;
+
+	 i = 0;
+
+	while
+		 (i < 10)
+		{
+			 j = 'a';
+			while
+				 (j <= 'z')
+				{
+					 _putchar(j);
+					j++;
+				}
+			 _putchar('\n');
+			 i++;
+		}
+}
+
