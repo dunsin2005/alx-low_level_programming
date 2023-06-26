dcadd22756d3a37f789e380ed2c2c98d21fcd863
Awotinde Racheal <dunsinawotinde2005@gmail.com>
2023-06-25 18:47:06 -0700

    prinys absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of an integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
